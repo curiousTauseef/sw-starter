e345ec7670b22743afdb1cab5b9019b3b6063667
williameliel <williameliel@gmail.com>
2017-01-31 16:49:45 -0500

    box

--- templates/modules/featured_text_content.php
+++ templates/modules/featured_text_content.php
@@ -1,6 +1,6 @@
 <!-- includes/<?php echo $layout['acf_fc_layout'] ?>.php  -->
 <div class="container">
-  <div class=" col-xs-12 col-md-6 col-centered">
+  <div class="col-centered">
      <?php include('clones/content_box.php'); ?>
   </div>
 </div>
